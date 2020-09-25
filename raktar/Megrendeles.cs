aec845bc0d8c58547a0e869161b62fb3f87cc132
palmandaniel <palman.daniel47@gmail.com>
2020-09-25 09:04:28 +0200

    Megrendeles osztaly tetelek nelkul

--- raktar/Megrendeles.cs
+++ raktar/Megrendeles.cs
@@ -8,5 +8,45 @@ namespace raktar
 {
     class Megrendeles
     {
-    }
+		private string datum;
+
+		public string DATUM
+		{
+			get { return datum; }
+			set { datum = value; }
+		}
+
+		private string id;
+
+		public string ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string email;
+
+		public string EMAIL
+		{
+			get { return email; }
+			set { email = value; }
+		}
+
+		private string rendeltek;
+
+		public string RENDELTEK
+		{
+			get { return rendeltek; }
+			set { rendeltek = value; }
+		}
+
+
+		public Megrendeles(string datum, string id, string email)
+		{
+			this.datum = datum;
+			this.id = id;
+			this.email = email;
+		}
+
+	}
 }
--- raktar/Program.cs
+++ raktar/Program.cs
@@ -10,10 +10,11 @@ namespace raktar
     class Program
     {
         static List<Termek> termekek = new List<Termek>();
+
+        static List<Megrendeles> rendelesek = new List<Megrendeles>();
         static void BeolvasRaktar()
         {
             StreamReader raktar = new StreamReader("raktar.csv");
-            
             while (!raktar.EndOfStream)
             {
                 //sor[0] kód
@@ -25,9 +26,24 @@ namespace raktar
                 termekek.Add(new Termek(sor[0], sor[1], int.Parse(sor[2]), int.Parse(sor[3])));
                 
             }
-
-
             raktar.Close();
+        }
+
+        static void BeolvasRendeles()
+        {
+            StreamReader rendeles = new StreamReader("rendeles.csv");
+            while (!rendeles.EndOfStream)
+            {
+                string[] sor = rendeles.ReadLine().Split(';');
+
+                if (sor[0] == "M")
+                {
+                    rendelesek.Add(new Megrendeles(sor[1], (sor[2]), sor[3]));
+                }
+
+                
+            }
+            rendeles.Close();
         }
 
         static void Main(string[] args)
@@ -39,7 +55,16 @@ namespace raktar
             {
                 Console.WriteLine(t.NEV);
             }
-            
+
+            BeolvasRendeles();
+
+            Console.WriteLine("**********************************************");
+
+            foreach (var t in rendelesek)
+            {
+                Console.Write("{0}\t {1}\t {2}\n", t.DATUM, t.ID, t.EMAIL);
+            }
+
             Console.ReadKey();
 
         }
